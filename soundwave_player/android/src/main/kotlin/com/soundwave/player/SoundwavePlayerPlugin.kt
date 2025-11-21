42c1bff720d16e738a345866798540128ea44501
qifugeng <qifu.geng@allsaintsmusic.com>
2025-11-21 14:21:15 +0800

    Story01: 添加 EventChannel 占位并完成任务5

--- soundwave_player/android/src/main/kotlin/com/soundwave/player/SoundwavePlayerPlugin.kt
+++ soundwave_player/android/src/main/kotlin/com/soundwave/player/SoundwavePlayerPlugin.kt
@@ -1,35 +1,66 @@
 package com.soundwave.player
 
-import androidx.annotation.NonNull
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 /** SoundwavePlayerPlugin */
-class SoundwavePlayerPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+class SoundwavePlayerPlugin : FlutterPlugin, MethodCallHandler, EventChannel.StreamHandler {
+  private lateinit var methodChannel: MethodChannel
+  private lateinit var stateEventChannel: EventChannel
+  private lateinit var pcmEventChannel: EventChannel
+  private lateinit var spectrumEventChannel: EventChannel
 
-  override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "soundwave_player")
-    channel.setMethodCallHandler(this)
-  }
+  override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+    methodChannel = MethodChannel(binding.binaryMessenger, METHOD_CHANNEL_NAME)
+    methodChannel.setMethodCallHandler(this)
 
-  override fun onMethodCall(call: MethodCall, result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
-    }
+    stateEventChannel = EventChannel(binding.binaryMessenger, "$EVENT_PREFIX/state")
+    pcmEventChannel = EventChannel(binding.binaryMessenger, "$EVENT_PREFIX/pcm")
+    spectrumEventChannel = EventChannel(binding.binaryMessenger, "$EVENT_PREFIX/spectrum")
+
+    stateEventChannel.setStreamHandler(this)
+    pcmEventChannel.setStreamHandler(this)
+    spectrumEventChannel.setStreamHandler(this)
   }
 
   override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
+    methodChannel.setMethodCallHandler(null)
+    stateEventChannel.setStreamHandler(null)
+    pcmEventChannel.setStreamHandler(null)
+    spectrumEventChannel.setStreamHandler(null)
+  }
+
+  override fun onMethodCall(call: MethodCall, result: Result) {
+    when (call.method) {
+      "init",
+      "load",
+      "play",
+      "pause",
+      "stop",
+      "seek" -> {
+        // Placeholder: succeed without doing anything yet
+        result.success(null)
+      }
+
+      else -> result.notImplemented()
+    }
+  }
+
+  // EventChannel.StreamHandler placeholders
+  override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
+    // No-op placeholder stream; actual event emission will be added in later tasks.
+  }
+
+  override fun onCancel(arguments: Any?) {
+    // No-op
+  }
+
+  companion object {
+    private const val METHOD_CHANNEL_NAME = "soundwave_player"
+    private const val EVENT_PREFIX = "soundwave_player/events"
   }
 }
--- soundwave_player/ios/Classes/SoundwavePlayerPlugin.swift
+++ soundwave_player/ios/Classes/SoundwavePlayerPlugin.swift
@@ -1,19 +1,47 @@
 import Flutter
 import UIKit
 
-public class SoundwavePlayerPlugin: NSObject, FlutterPlugin {
+public class SoundwavePlayerPlugin: NSObject, FlutterPlugin, FlutterStreamHandler {
+  private var methodChannel: FlutterMethodChannel?
+  private var stateEventChannel: FlutterEventChannel?
+  private var pcmEventChannel: FlutterEventChannel?
+  private var spectrumEventChannel: FlutterEventChannel?
+
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "soundwave_player", binaryMessenger: registrar.messenger())
     let instance = SoundwavePlayerPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
+    instance.methodChannel = FlutterMethodChannel(name: methodChannelName, binaryMessenger: registrar.messenger())
+    registrar.addMethodCallDelegate(instance, channel: instance.methodChannel!)
+
+    instance.stateEventChannel = FlutterEventChannel(name: "\(eventPrefix)/state", binaryMessenger: registrar.messenger())
+    instance.pcmEventChannel = FlutterEventChannel(name: "\(eventPrefix)/pcm", binaryMessenger: registrar.messenger())
+    instance.spectrumEventChannel = FlutterEventChannel(name: "\(eventPrefix)/spectrum", binaryMessenger: registrar.messenger())
+
+    instance.stateEventChannel?.setStreamHandler(instance)
+    instance.pcmEventChannel?.setStreamHandler(instance)
+    instance.spectrumEventChannel?.setStreamHandler(instance)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
+    case "init", "load", "play", "pause", "stop", "seek":
+      // Placeholder: succeed without actual implementation yet.
+      result(nil)
     default:
       result(FlutterMethodNotImplemented)
     }
   }
+
+  // MARK: - FlutterStreamHandler placeholder
+  public func onListen(withArguments arguments: Any?, eventSink events: @escaping FlutterEventSink) -> FlutterError? {
+    // No-op placeholder for now.
+    return nil
+  }
+
+  public func onCancel(withArguments arguments: Any?) -> FlutterError? {
+    // No-op placeholder for now.
+    return nil
+  }
+
+  private static let methodChannelName = "soundwave_player"
+  private static let eventPrefix = "soundwave_player/events"
 }
